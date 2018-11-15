99748bf01146f6b1b3555427eec5edd76b0b471c
Kim In Seop <opnayn@gmail.com>
2018-11-15 17:28:28 +0900

    디바이스 화면 삭제
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

--- router/main.js
+++ router/main.js
@@ -4,17 +4,7 @@
  *          conn.execute(sql, func(err, row, fields))
  */
 module.exports = function(app, conn) {
-    var data = { devices: [
-        // {이름, 상태, 배터리}
-        {name: "장치 1", status: "on", battery: 100},
-        {name: "장치 2", status: "off", battery: -1},
-        {name: "장치 3", status: "on", battery: 80},
-        {name: "장치 4", status: "on", battery: 90},
-        {name: "장치 5", status: "on", battery: 33},
-        {name: "장치 6", status: "on", battery: 65},
-        {name: "장치 7", status: "on", battery: 42},
-        {name: "장치 8", status: "off", battery: -1}
-    ]};
+    var data = {name: "장치 1", status: "on", battery: 100};
 
     app.get('/', function(req, res) {
         res.render('index', {
@@ -28,7 +18,7 @@ module.exports = function(app, conn) {
         res.render('device', {
             title: "장치 화면",
             index: req.params.idx,
-            data: data.devices[req.params.idx]
+            data: data
         });
     });
 
